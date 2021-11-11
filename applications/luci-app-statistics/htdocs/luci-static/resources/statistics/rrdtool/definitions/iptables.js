25c5ad7276b73696745b918531855cd5fae122ae
Alexey Smirnov <s.alexey@gmail.com>
2021-11-11 16:24:53 +0300

    luci-app-statistics: change units from bytes/s to bits/s
    Signed-off-by: Alexey Smirnov <s.alexey@gmail.com>

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/iptables.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/iptables.js
@@ -8,23 +8,24 @@ return baseclass.extend({
 
 	rrdargs: function(graph, host, plugin, plugin_instance, dtype) {
 		return [{
-			title: "%H: Firewall: Processed bytes in %pi",
-			vlabel: "Bytes/s",
-			number_format: "%5.1lf%sB/s",
-			totals_format: "%5.1lf%sB",
+			title: "%H: Firewall: Processed bits in %pi",
+			vlabel: "Bits/s",
+			number_format: "%5.1lf%sb/s",
+			totals_format: "%5.1lf%sb",
 			data: {
 				types: [ "ipt_bytes" ],
 				options: {
 					ipt_bytes: {
 						total: true,
-						title: "%di"
+						title: "%di",
+						transform_rpn: "8,*"
 					}
 				}
 			}
 		}, {
 			title: "%H: Firewall: Processed packets in %pi",
 			vlabel: "Packets/s",
-			number_format: "%5.1lf P/s",
+			number_format: "%5.1lf%sP/s",
 			totals_format: "%5.1lf%s",
 			data: {
 				types: [ "ipt_packets" ],
