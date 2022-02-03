b5bf6403b266ab25613b1204411353079384af7e
Alexey Smirnov <s.alexey@gmail.com>
2022-02-03 11:58:40 +0300

    luci-mod-status: add eMMC status
    Signed-off-by: Alexey Smirnov <s.alexey@gmail.com>

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/15_am335x.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/15_am335x.js
@@ -7,7 +7,9 @@ return baseclass.extend({
 
 	load: function() {
 		return Promise.all([
-			fs.read_direct('/tmp/sysinfo/am335x_info.json', 'json')
+			fs.read_direct('/tmp/sysinfo/am335x_info.json', 'json'),
+			fs.trimmed('/sys/class/mmc_host/mmc1/mmc1:0001/life_time'),
+			fs.trimmed('/sys/class/mmc_host/mmc1/mmc1:0001/pre_eol_info')
 		]);
 	},
 
@@ -33,6 +35,35 @@ return baseclass.extend({
 			return "Unknown reason: 0x%08X".format(val);
 	},
 
+	emmcEOL: function(eol) {
+		var text = "";
+		var color = null
+		if(eol == 1)
+			text = 'Normal: consumed < 80% of the reserved blocks';
+		else if(eol == 2) {
+			text = 'Warning: consumed 80% of the reserved blocks';
+			color = 'color:orange';
+		}
+		else if(eol == 3) {
+			text = 'Urgent: consumed 90% of the reserved blocks';
+			color = 'color:red';
+		}
+		else
+			text = 'undefined';
+
+		return E('td', { 
+				'style' : color
+				}, _(text) );
+	},
+
+	emmcLifeTime: function(s) {
+		var m = s.split(/\s+/);
+
+		return { slc : parseInt(m[0], 16),
+				 mlc : parseInt(m[1], 16)
+				};
+	},
+
 	render: function(data) {
 		var bootcount = data[0].hasOwnProperty("boot_count_reg") ? 
 						this.swap32(+data[0].boot_count_reg) & 0xFFFF : "Unknown";
@@ -40,9 +71,15 @@ return baseclass.extend({
 		var reset_reg = data[0].hasOwnProperty("reset_reason_reg") ? 
 						this.reason_str(+data[0].reset_reason_reg) : "Unknown";
 
+		var emmcLt = this.emmcLifeTime(data[1]);
+		var emmEol = this.emmcEOL(parseInt(data[2], 16));
+
 		var fields = [
 			_('Boot count'),       bootcount,
-			_('Reset reason'),     reset_reg
+			_('Reset reason'),     reset_reg,
+			_('eMMC life time used for SLC eraseblocks'), '%d - %d\%'.format((emmcLt.slc -1) * 10, emmcLt.slc * 10),
+			_('eMMC life time used for MLC eraseblocks'), '%d - %d\%'.format((emmcLt.mlc -1) * 10, emmcLt.slc * 10),
+			_('eMMC status for reserved blocks'), emmEol
 		];
 
 		var table = E('table', { 'class': 'table' });
